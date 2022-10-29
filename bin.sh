73c773c4b66e899f768b681b032bbec5c63ea5d6
udaysai <112326056+reddyudaysai123@users.noreply.github.com>
2022-10-29 08:36:02 +0530

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -1,9 +1,13 @@
 !#/bin/bash
 
-az group create --name 'uday' --location 'westus'
+az group create --name 'uday' --location 'central india'
 
-az network vnet create --name 'fromcli' --resource-group 'uday' --address-prefixes '192.168.0.0/16' --location 'westus'
+az network vnet create --name 'fromcli' --resource-group 'qt' --address-prefixes '192.168.0.0/16' --location 'central india'
 
-az network vnet subnet create --address-prefixes '192.168.0.0/24' --name 'subnet1'  --resource-group 'uday' --vnet-name 'fromcli' --network-security-group Nsg
+az network vnet subnet create --address-prefixes '192.168.0.0/24' --name 'subnet1' --resource-group 'qt' --vnet-name 'fromcli'
 
-az vm create  --resource-group 'uday'   --name 'myvm' --image 'UbuntuLTS'  --vnet-name 'fromcli' --subnet  'subnet' --generate-ssh-keys
+az network nsg create --name 'Nsg' --resource-group 'qt' --location 'central india'
+
+az network nsg rule create --name 'mynsg' --nsg-name 'Nsg' --priority '100' --resource-group 'uday' --access 'Allow' --destination-port-ranges '8080' --direction 'Inbound' --protocol 'Tcp'
+
+ az vm create --name 'aznode' --resource-group 'qt' --image 'ubuntuLTS' --admin-username 'azure' --admin-password 'devopsazure@1998'
